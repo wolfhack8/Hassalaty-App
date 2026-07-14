9ce5f4eead00564cacaaaef4ded1172c5d93e191
wolfhack <balkhlfy08@gmail.com>
2026-07-14 23:12:42 +0300

    Delete route (4).ts

--- route (4).ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { auth } from "@/auth";
-
-export default async function Home() {
-  const session = await auth();
-  if (!session?.user) redirect("/login");
-  redirect(session.user.role === "CHILD" ? "/child" : "/parent");
-}
